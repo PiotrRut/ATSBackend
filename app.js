0dcf1213bdc0418f592826eb1d449e96815e60cb
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-01-27 12:16:55 +0000

    MongoDB connection established

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const passport = require('passport')
+require('dotenv').config()
 const app = express()
-const port =  process.env.PORT || 3001
+const port =  process.env.PORT || 5000
+
+const db = mongoose.connect(process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.get('/', (req, res) => res.send('API is working correctly!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
