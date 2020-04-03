06545286945c842e0edf3e82dea0c6ba9be6e320
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-04-04 01:18:49 +0200

    Saving before breaking

--- dbRoutes/blanks.js
+++ dbRoutes/blanks.js
@@ -3,8 +3,15 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const Blank = require('../schemas/Blank')
+const {performance} = require('perf_hooks');
 
-// Used to add a new rate to the system
+/*
+* Used to add a new range of blanks to the system.
+* `to` and `from` define the upper and lower bounds of the range.
+* Individual blank records are then created withing that range,
+* each with unique ID's and void status (defaulted to false)
+*/
+
 router.post('/addBlanks', async (req, res, next) => {
   jwt.verify(req.query.secret_token, process.env.JWT_SECRET, async (err, decoded) => {
     if (decoded.user.role == 'Admin') {
@@ -17,7 +24,8 @@ router.post('/addBlanks', async (req, res, next) => {
           void: req.body.void
         })
         console.log(blanks[blanks.length -1])
-      } res.send('Added successfully');
+      }
+      res.send('Added successfully');
     } else {
       res.status(401).json({ message: 'Unauthorised' })
     }
