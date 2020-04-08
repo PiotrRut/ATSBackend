a375dfa72e7f1519e214c6e12a994187130f145a
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-04-08 23:42:57 +0200

    Backup and restore function with mongodump/mongorestore

--- dbRoutes/backup.js
+++ dbRoutes/backup.js
@@ -1,23 +1,43 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 const User = require('../schemas/User');
+const Blank = require('../schemas/Blank');
+const Customer = require('../schemas/Customer');
+const CommissionRate = require('../schemas/CommissionRate');
+const ExchangeRate = require('../schemas/ExchangeRate');
+const TravelAgent = require('../schemas/TravelAgent');
+const PaymentCard = require('../schemas/PaymentCard');
 const fs = require('fs');
-const CommissionRate = require('../schemas/CommissionRate')
 
-const { exec } = require("child_process");
+const { spawn, exec } = require('child_process');
 
-router.post('/backup', (req, res) => {
-    User.find({}, (err, docs) => {
-      if (err)
-        return res.sendStatus(500)
-      fs.writeFileSync('output.json', JSON.stringify(docs, null, 2), {flag: 'a'}, function(){ return res.json(docs) })
-    })
-    CommissionRate.find({}, (err, docs) => {
-      if (err)
-        return res.sendStatus(500)
-      fs.writeFileSync('output.json', JSON.stringify(docs, null, 2), {flag: 'a'}, function(){ return res.json(docs) })
-    })
+// Creating a dump of the database in the dump directory of the server, along with all the db metadata
+router.post('/backupdb', (req, res) => {
+  jwt.verify(req.query.secret_token, process.env.JWT_SECRET, async (err, decoded) => {
+    if (decoded.user.role == 'Admin') {
+        var backupDB = exec('mongodump --host ' + process.env.MONGO_HOST + ' --ssl --username ' + process.env.MONGO_USNM + ' --password ' + process.env.MONGO_PW + ' --authenticationDatabase admin --db ATS');
+            backupDB.stdout.on('data',function(data){
+                console.log('stdout: ' + data);
+            });
+    }
+  })
 })
+
+// Restoring the database from latest dump
+router.post('/restoredb', (req, res) => {
+  jwt.verify(req.query.secret_token, process.env.JWT_SECRET, async (err, decoded) => {
+    if (decoded.user.role == 'Admin') {
+        var backupDB = exec('mongorestore --host ' + process.env.MONGO_HOST + ' --ssl --username ' + process.env.MONGO_USNM + ' --password ' + process.env.MONGO_PW + ' --authenticationDatabase admin');
+            backupDB.stdout.on('data',function(data){
+                console.log('stdout: ' + data);
+            });
+    }
+  })
+})
+
+
+
 
 module.exports = router
